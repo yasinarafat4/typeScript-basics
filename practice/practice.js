72258ef8804901cfa673df4e1ca186c059f49007
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-11-06 12:48:03 +0600

    Array Methods added

--- practice/practice.js
+++ practice/practice.js
@@ -4,9 +4,51 @@
     var age = 22;
     var isMale = true;
     var fullName = firstName.concat(lastName);
-    console.log("This is ".concat(fullName, ". I am ").concat(age, " yars old. Gender Male : ").concat(isMale));
+    // console.log(
+    //   `This is ${fullName}. I am ${age} yars old. Gender Male : ${isMale}`
+    // );
     // We can do lost of work on the variable. For example:
-    console.log(fullName.split(" "));
-    console.log(fullName.toUpperCase());
-    console.log(fullName.toLocaleLowerCase());
+    // console.log(fullName.split(" "));
+    // console.log(fullName.toUpperCase());
+    // console.log(fullName.toLocaleLowerCase());
+    // Array types
+    var persons = ["Yasin", "Arafat", "Hasib"];
+    // console.log(persons);
+    // or we can use Array<string>
+    var colors = ["Red", "Green", "Blue"];
+    // console.log(colors);
+    // Multiple types (union)
+    var multiple = ["Yasin", 111, true];
+    // console.log(multiple);
+    var multipleUnion = [
+        "Arafat",
+        444,
+        false,
+    ];
+    // console.log(multipleUnion);
+    // Array methods
+    // for
+    for (var i = 0; i < persons.length; i++) {
+        var result = persons[i];
+        // console.log(result);
+    }
+    // forEach
+    persons.forEach(function (person) {
+        // console.log(person);
+    });
+    // sort
+    persons.sort();
+    // console.log(persons);
+    // push
+    persons.push("Jeri");
+    // console.log(persons);
+    // pop
+    persons.pop();
+    // console.log(persons);
+    // shift
+    persons.shift();
+    // console.log(persons);
+    // unshift
+    persons.unshift("Habib");
+    console.log(persons);
 }
--- practice/practice.ts
+++ practice/practice.ts
@@ -5,12 +5,64 @@
   const isMale: boolean = true;
   const fullName = firstName.concat(lastName);
 
-  console.log(
-    `This is ${fullName}. I am ${age} yars old. Gender Male : ${isMale}`
-  );
+  // console.log(
+  //   `This is ${fullName}. I am ${age} yars old. Gender Male : ${isMale}`
+  // );
 
   // We can do lost of work on the variable. For example:
-  console.log(fullName.split(" "));
-  console.log(fullName.toUpperCase());
-  console.log(fullName.toLocaleLowerCase());
+  // console.log(fullName.split(" "));
+  // console.log(fullName.toUpperCase());
+  // console.log(fullName.toLocaleLowerCase());
+
+  // Array types
+  const persons: string[] = ["Yasin", "Arafat", "Hasib"];
+  // console.log(persons);
+
+  // or we can use Array<string>
+  const colors: Array<string> = ["Red", "Green", "Blue"];
+  // console.log(colors);
+
+  // Multiple types (union)
+  const multiple: (number | string | boolean)[] = ["Yasin", 111, true];
+  // console.log(multiple);
+
+  const multipleUnion: Array<number | string | boolean> = [
+    "Arafat",
+    444,
+    false,
+  ];
+  // console.log(multipleUnion);
+
+  // Array methods
+
+  // for
+  for (let i = 0; i < persons.length; i++) {
+    const result = persons[i];
+    // console.log(result);
+  }
+
+  // forEach
+  persons.forEach((person) => {
+    // console.log(person);
+  });
+
+  // sort
+  persons.sort();
+  // console.log(persons);
+
+  // push
+  persons.push("Jeri");
+  // console.log(persons);
+
+  // pop
+  persons.pop();
+  // console.log(persons);
+
+  // shift
+  persons.shift();
+  // console.log(persons);
+
+  // unshift
+  persons.unshift("Habib");
+  console.log(persons);
 }
